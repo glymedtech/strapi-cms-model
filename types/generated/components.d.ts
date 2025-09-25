66dd82f53924f1f77251bad6ceec1e92af68a7c2
HMadhav <madhav1993hari@gmail.com>
2025-09-25 13:26:34 +0530

    add new section

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,53 @@
 import type { Schema, Struct } from '@strapi/strapi';
 
+export interface SharedBanner extends Struct.ComponentSchema {
+  collectionName: 'components_shared_banners';
+  info: {
+    displayName: 'Banner';
+  };
+  attributes: {
+    AlignmentOnDesktop: Schema.Attribute.Enumeration<
+      ['left', 'right', 'center']
+    > &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<'left'>;
+    AlignmentOnMobile: Schema.Attribute.Enumeration<
+      ['top', 'center', 'bottom']
+    > &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<'top'>;
+    BannerSubTitle: Schema.Attribute.Component<'shared.text-with-color', false>;
+    BannerTitle: Schema.Attribute.Component<'shared.text-with-color', false>;
+    ButtonColor: Schema.Attribute.String &
+      Schema.Attribute.CustomField<'plugin::color-picker.color'>;
+    ButtonColorOnHover: Schema.Attribute.String &
+      Schema.Attribute.CustomField<'plugin::color-picker.color'>;
+    ButtonLink: Schema.Attribute.String;
+    ButtonText: Schema.Attribute.String;
+    ButtonTextColor: Schema.Attribute.String &
+      Schema.Attribute.CustomField<'plugin::color-picker.color'>;
+    ButtonTextColorOnHover: Schema.Attribute.String &
+      Schema.Attribute.CustomField<'plugin::color-picker.color'>;
+    CTAText: Schema.Attribute.Component<'shared.text-with-color', false>;
+    desktop_1440: Schema.Attribute.Media<'images'>;
+    EndDate: Schema.Attribute.DateTime;
+    IsExternalLink: Schema.Attribute.Boolean &
+      Schema.Attribute.DefaultTo<false>;
+    IsRegularTitle: Schema.Attribute.Boolean &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<true>;
+    laptop_1024: Schema.Attribute.Media<'images'> & Schema.Attribute.Required;
+    mobile_375: Schema.Attribute.Media<'images' | 'files'>;
+    mobile_480: Schema.Attribute.Media<'images'>;
+    PageType: Schema.Attribute.String & Schema.Attribute.Required;
+    StartDate: Schema.Attribute.DateTime;
+    SubTitleFontSizeDesktop: Schema.Attribute.Integer &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<18>;
+    tablet_768: Schema.Attribute.Media<'images'>;
+  };
+}
+
 export interface SharedListItem extends Struct.ComponentSchema {
   collectionName: 'components_shared_list_items';
   info: {
@@ -8,17 +56,6 @@ export interface SharedListItem extends Struct.ComponentSchema {
   };
   attributes: {
     Text: Schema.Attribute.String & Schema.Attribute.Required;
-  };
-}
-
-export interface SharedMedia extends Struct.ComponentSchema {
-  collectionName: 'components_shared_media';
-  info: {
-    displayName: 'Media';
-    icon: 'file-video';
-  };
-  attributes: {
-    file: Schema.Attribute.Media<'images' | 'files' | 'videos'>;
   };
 }
 
@@ -50,35 +87,11 @@ export interface SharedProvenResult extends Struct.ComponentSchema {
   };
 }
 
-export interface SharedQuote extends Struct.ComponentSchema {
-  collectionName: 'components_shared_quotes';
-  info: {
-    displayName: 'Quote';
-    icon: 'indent';
-  };
-  attributes: {
-    body: Schema.Attribute.Text;
-    title: Schema.Attribute.String;
-  };
-}
-
-export interface SharedRichText extends Struct.ComponentSchema {
-  collectionName: 'components_shared_rich_texts';
-  info: {
-    description: '';
-    displayName: 'Rich text';
-    icon: 'align-justify';
-  };
-  attributes: {
-    body: Schema.Attribute.RichText;
-  };
-}
-
 export interface SharedSeo extends Struct.ComponentSchema {
   collectionName: 'components_shared_seos';
   info: {
     description: '';
-    displayName: 'Seo';
+    displayName: 'MetaTag';
     icon: 'allergies';
     name: 'Seo';
   };
@@ -86,18 +99,6 @@ export interface SharedSeo extends Struct.ComponentSchema {
     metaDescription: Schema.Attribute.Text & Schema.Attribute.Required;
     metaTitle: Schema.Attribute.String & Schema.Attribute.Required;
     shareImage: Schema.Attribute.Media<'images'>;
-  };
-}
-
-export interface SharedSlider extends Struct.ComponentSchema {
-  collectionName: 'components_shared_sliders';
-  info: {
-    description: '';
-    displayName: 'Slider';
-    icon: 'address-book';
-  };
-  attributes: {
-    files: Schema.Attribute.Media<'images', true>;
   };
 }
 
@@ -117,7 +118,7 @@ export interface SharedTestimonials extends Struct.ComponentSchema {
 export interface SharedTextWithColor extends Struct.ComponentSchema {
   collectionName: 'components_shared_text_with_colors';
   info: {
-    displayName: 'Text With Color';
+    displayName: 'TextWithColor';
   };
   attributes: {
     Color: Schema.Attribute.String &
@@ -127,19 +128,44 @@ export interface SharedTextWithColor extends Struct.ComponentSchema {
   };
 }
 
+export interface SharedWhyChooseGlyMedPlus extends Struct.ComponentSchema {
+  collectionName: 'components_shared_why_choose_gly_med_pluses';
+  info: {
+    displayName: 'WhyChooseGlyMedPlus';
+  };
+  attributes: {
+    ButtonAlignment: Schema.Attribute.Enumeration<['left', 'center', 'right']> &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<'right'>;
+    ButtonText: Schema.Attribute.String;
+    Description: Schema.Attribute.Component<'shared.text-with-color', false>;
+    Desktop: Schema.Attribute.Media<'images'>;
+    Desktop2x: Schema.Attribute.Media<'images'>;
+    IsExternalLink: Schema.Attribute.Boolean &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<false>;
+    LinkURL: Schema.Attribute.String;
+    Portrait: Schema.Attribute.Media<'images'>;
+    Portrait2x: Schema.Attribute.Media<'images'>;
+    SliderName: Schema.Attribute.String & Schema.Attribute.Required;
+    TextAlignment: Schema.Attribute.Enumeration<['left', 'center', 'right']> &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<'left'>;
+    Title: Schema.Attribute.Component<'shared.text-with-color', false>;
+  };
+}
+
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
+      'shared.banner': SharedBanner;
       'shared.list-item': SharedListItem;
-      'shared.media': SharedMedia;
       'shared.product-card': SharedProductCard;
       'shared.proven-result': SharedProvenResult;
-      'shared.quote': SharedQuote;
-      'shared.rich-text': SharedRichText;
       'shared.seo': SharedSeo;
-      'shared.slider': SharedSlider;
       'shared.testimonials': SharedTestimonials;
       'shared.text-with-color': SharedTextWithColor;
+      'shared.why-choose-gly-med-plus': SharedWhyChooseGlyMedPlus;
     }
   }
 }

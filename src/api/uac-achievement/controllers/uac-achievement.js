da180d91f81c46f03f412732147491308aacd1a8
HMadhav <madhav1993hari@gmail.com>
2025-10-15 10:34:34 +0530

    add UAC Page

---
+++ src/api/uac-achievement/controllers/uac-achievement.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * uac-achievement controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::uac-achievement.uac-achievement');
---
+++ src/api/uac-page/controllers/uac-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * uac-page controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::uac-page.uac-page');
